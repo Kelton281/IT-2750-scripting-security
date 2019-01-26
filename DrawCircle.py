5c7cd6193131ccffb30ac918a4bb102932d6b2b6
Kelton281 <Greenwoodkelton@gmail.com>
2019-01-25 20:04:52 -0500

    hi

---
+++ DrawCircle.py
@@ -0,0 +1,14 @@
+import turtle
+ThismyTurtle = turtle.Turtle()
+ThismyTurtle.circle(50)
+
+ThismyTurtle.penup()
+ThismyTurtle.setposition(-140, 0)
+ThismyTurtle.pendown()
+ThismyTurtle.circle(50)
+
+ThismyTurtle.penup()
+ThismyTurtle.setposition(140, 0)
+ThismyTurtle.pendown()
+ThismyTurtle.circle(50)
+turtle.getscreen()._root.mainloop()
---
+++ Loops.py
@@ -0,0 +1,2 @@
+sequence = list(range(1,10,2)) + list range(20,15,-1)) + list(range(0,100,2))
+print(sequence)

8056c6ae16605c18437a3be90abe381735610294
Kelton281 <46682417+Kelton281@users.noreply.github.com>
2019-03-03 16:53:13 -0500

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,100 @@
+#7.1
+#def myfunc():
+    #total = 0
+    #while total < 50:
+        #total = total + 1
+        #print(total)
+#myfunc()
+#7.2
+#def myfunc():
+ #   hi = "Hello world"
+  #  bye = "bye world"
+   # while (len(hi)) < 50:
+    #    endingstring = hi + bye
+     #   endingstring.upper
+      #  print(endingstring)
+#myfunc()
+#7.3
+#def myfunc():
+    #sum = 0
+    #examscore = input("enter your exam score")
+    #val = int(examscore)
+    #amountofinputs = 0
+    #while examscore:
+        #examscore = input("enter your exam score")
+        #sum = sum + val
+        #amountofinputs = amountofinputs + 1
+        #print("your new exam score", sum)
+        #if examscore == ["stop"]:
+            #newsum = sum / amountofinputs
+            #print(newsum)
+            #break
+#myfunc()
+#7.4
+#def mypalindrome(word):
+    #for i in range(len(word)//2):
+        #if word[i] != word[-1-i]:
+            #return False
+    #return True
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+        
+
+
+        
+
+
+  
+
+    
+    
+
+        
+
+
+        
+
+        
+
+
+
+        
+        
+
+        
+
+
+         
+        
+
+    
+         
+
+    
+  
+
+    
+
+
+        

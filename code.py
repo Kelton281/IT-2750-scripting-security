3ea8998049d264c7180829e20ade05162b401611
Kelton281 <46682417+Kelton281@users.noreply.github.com>
2019-03-05 17:55:40 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,21 @@
+#8.9
+#8.14
+#def wordpop(text, n):
+    #nwords = []
+    #words = text.split()
+    #for word in words:
+        #if (len(word) >=n):
+            #nwords.append(word)
+    #return sorted(nwords, key=len, reverse= True)
+#text =("please do not sort this text")
+#print (wordpop(text.lower(), 4))
+#8.18
+#import re
+#userinput = input("enter a expression")
+#print(re.findall(r"\b(\w+ing)\b", userinput))
+#8.20
+#import re
+#sentence = "all and all for one"
+#findpattren = r"\ba[^\W\d_]*a\b"
+#print(re.findall(findpattren, sentence))
+
